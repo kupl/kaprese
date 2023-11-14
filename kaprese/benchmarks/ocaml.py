11f4e698b4705a0bffe9a9cdf9d55d60caafef6b
Henry Lee <gbvrcx@gmail.com>
2023-11-14 01:55:41 +0000

    feat: language from container

--- kaprese/benchmarks/ocaml.py
+++ kaprese/benchmarks/ocaml.py
@@ -1,29 +1,35 @@
 from kaprese.core.benchmark import Benchmark
+from kaprese.utils.logging import logger
 
 
 def register_benchmarks(overwrite: bool = False) -> None:
     formulas = [
         Benchmark(
             f"formula-{i}",
-            "ocaml",
             f"ghcr.io/kupl/starlab-benchmarks/ocaml:formula-{i}",
+            language_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cat metadata.json | jq -r .language",
         )
         for i in range(1, 101)
     ]
 
     diffs = [
         Benchmark(
-            f"diff-{i}", "ocaml", f"ghcr.io/kupl/starlab-benchmarks/ocaml:diff-{i}"
+            f"diff-{i}",
+            f"ghcr.io/kupl/starlab-benchmarks/ocaml:diff-{i}",
+            language_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cat metadata.json | jq -r .language",
         )
         for i in range(1, 101)
     ]
 
     lambdas = [
         Benchmark(
-            f"lambda-{i}", "ocaml", f"ghcr.io/kupl/starlab-benchmarks/ocaml:lambda-{i}"
+            f"lambda-{i}",
+            f"ghcr.io/kupl/starlab-benchmarks/ocaml:lambda-{i}",
+            language_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cat metadata.json | jq -r .language",
         )
         for i in range(1, 101)
     ]
 
     for bench in [*formulas, *diffs, *lambdas]:
+        logger.info(f"Registering ocaml benchmark {bench.name}")
         bench.register(overwrite=overwrite)
--- kaprese/core/benchmark.py
+++ kaprese/core/benchmark.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
+
 import dataclasses
 import json
 from pathlib import Path
 
 from kaprese.core.config import CONFIGURE
-from kaprese.utils.docker import find_image
+from kaprese.utils.docker import image_exists, run_command
 from kaprese.utils.logging import logger
 
 
@@ -14,12 +16,22 @@ def _get_benchmark_path() -> Path:
 @dataclasses.dataclass
 class Benchmark:
     name: str
-    language: str
     image: str
+    _language: str | None = None
+    language_command: str | None = dataclasses.field(default=None, repr=False)
+
+    @property
+    def language(self) -> str | None:
+        if self._language is None and self.availability and image_exists(self.image):
+            out = run_command(self.image, self.language_command)
+            if out is not None:
+                out = out.strip()
+            self._language = out
+        return self._language
 
     @property
     def availability(self) -> bool:
-        return find_image(self.image) is not None
+        return image_exists(self.image)
 
     def register(self, *, overwrite: bool = False) -> None:
         benchmarks_dir = _get_benchmark_path()
@@ -31,15 +43,38 @@ class Benchmark:
             if not overwrite:
                 return
             logger.warning("Overwriting benchmark")
-        benchmark_file.write_text(json.dumps(dataclasses.asdict(self)))
+        self.save(benchmark_file)
+
+    def unregister(self) -> None:
+        benchmark_file = _get_benchmark_path() / f"{self.name}.json"
+        if not benchmark_file.exists():
+            logger.warning(f"Benchmark {self.name} does not exist")
+            return
+        benchmark_file.unlink()
+
+    @classmethod
+    def load(cls, name: str) -> Benchmark | None:
+        benchmark_file = _get_benchmark_path() / f"{name}.json"
+        if not benchmark_file.exists():
+            logger.warning(f"Benchmark {name} does not exist")
+            return None
+        return cls(**json.loads(benchmark_file.read_text()))
+
+    def save(self, path: Path | str) -> None:
+        path = Path(path)
+        benchmark = dataclasses.asdict(self)
+        if benchmark["_language"] is None:
+            del benchmark["_language"]
+        path.write_text(json.dumps(benchmark))
 
 
 def all_benchmarks(path: Path | None = None) -> list[Benchmark]:
     path = path or _get_benchmark_path()
     return (
         [
-            Benchmark(**json.loads(benchmark_file.read_text()))
+            benchmark
             for benchmark_file in path.glob("*.json")
+            if (benchmark := Benchmark.load(benchmark_file.stem)) is not None
         ]
         if path.exists()
         else []
