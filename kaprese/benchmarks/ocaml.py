15da98b1478ac2a90560e2eab2425b20cb210eff
Henry Lee <gbvrcx@gmail.com>
2023-11-22 02:01:31 +0000

    fix: restore benchmark name

--- kaprese/benchmarks/ocaml.py
+++ kaprese/benchmarks/ocaml.py
@@ -23,9 +23,9 @@ def register_benchmarks(overwrite: bool = False) -> None:
         for i in range(1, 101)
     ]
 
-    checks = [
+    lambdas = [
         Benchmark(
-            f"check-{i}",
+            f"lambda-{i}",
             f"ghcr.io/kupl/starlab-benchmarks/ocaml:lambda-{i}",
             language_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cat metadata.json | jq -r .language",
             workdir_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cd $(cat metadata.json | jq -r .buggyPath) && pwd",
@@ -33,6 +33,6 @@ def register_benchmarks(overwrite: bool = False) -> None:
         for i in range(1, 101)
     ]
 
-    for bench in [*formulas, *diffs, *checks]:
+    for bench in [*formulas, *diffs, *lambdas]:
         logger.info(f"Registering ocaml benchmark {bench.name}")
         bench.register(overwrite=overwrite)
