26f5840d123fb354fe59cfaed160596441659a79
Henry Lee <gbvrcx@gmail.com>
2023-11-22 00:40:48 +0000

    fix: fix cafe commands

--- kaprese/engines/cafe.py
+++ kaprese/engines/cafe.py
@@ -13,8 +13,8 @@ def register_cafe(overwrite: bool = False) -> None:
         },
         exec_commands=[
             "set -o pipefail",
-            "export PROBLEM_NAME=$(echo {benchmark.name} | cut -d'-' -f1)"
-            "/opt/LearnML/engine/cafe -fix -solutions /opt/LearnML/benchmarks/C/${{PROBLEM_NAME}} "
+            "export PROBLEM_NAME=$(echo {benchmark.name} | cut -d'-' -f1)",
+            "/opt/LearnML/engine/main.native -fix -solutions /opt/LearnML/benchmarks/C/${{PROBLEM_NAME}} "
             "-submission src.ml -testcases testcases "
             '$([-e test.ml ] && \\"-entry grading -grading test.ml\\" || echo \\"-entry ${{PROBLEM_NAME}}\\") '
             "| tee {runner.mount_dir}/cafe.log",
