53cbdef15cbe561566abe8397376bf0ecf052992
Henry Lee <gbvrcx@gmail.com>
2023-11-23 05:25:13 +0000

    feat: save after prepare

--- kaprese/bin/kaprese_run.py
+++ kaprese/bin/kaprese_run.py
@@ -352,8 +352,11 @@ def main(
             # Start checking
             row.check_start()
             if not bench.availability:
-                logger.info('Benchmark "%s" is not available, try to prepare it', bench.name)
+                logger.info(
+                    'Benchmark "%s" is not available, try to prepare it', bench.name
+                )
                 bench.prepare()
+                bench.save()
             support_check = engine.support(bench)
             row.check_done(support_check)
             if not support_check:
