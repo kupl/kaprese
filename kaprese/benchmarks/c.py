5fd9f867715526fca040de99ee07c340bc4b08c0
오원석 <wonseok@owonseog-ui-iMac.local>
2024-11-18 14:31:35 +0900

    add unregister benchmark

--- kaprese/benchmarks/c.py
+++ kaprese/benchmarks/c.py
@@ -36,3 +36,38 @@ def register_benchmarks(overwrite: bool = False) -> None:
     for bench in [*flexs, *flints, *spearmints]:
         logger.info(f"Registering c benchmark {bench.name}")
         bench.register(overwrite=overwrite)
+
+def unregister_benchmarks(delete_image: bool = False) -> None:
+    flexs = [
+        Benchmark(
+            f"flex-{i}",
+            f"ghcr.io/kupl/starlab-benchmarks/c:flex-{i}",
+            language_command="cat metadata.json | jq -r .language",
+            workdir_command="cd $(cat metadata.json | jq -r .buggyPath) && pwd",
+        )
+        for i in range(1, 7)
+    ]
+
+    flints = [
+        Benchmark(
+            f"flint-{i}",
+            f"ghcr.io/kupl/starlab-benchmarks/c:flint-{i}",
+            language_command="cat metadata.json | jq -r .language",
+            workdir_command="cd $(cat metadata.json | jq -r .buggyPath) && pwd",
+        )
+        for i in range(1, 2)
+    ]
+
+    spearmints = [
+        Benchmark(
+            f"spearmint-{i}",
+            f"ghcr.io/kupl/starlab-benchmarks/c:spearmint-{i}",
+            language_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cat metadata.json | jq -r .language",
+            workdir_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cd $(cat metadata.json | jq -r .buggyPath) && pwd",
+        )
+        for i in range(1, 2)
+    ]
+
+    for bench in [*flexs, *flints, *spearmints]:
+        logger.info(f"Unregistering c benchmark {bench.name}")
+        bench.unregister(delete_image=delete_image)
--- kaprese/benchmarks/ocaml.py
+++ kaprese/benchmarks/ocaml.py
@@ -36,3 +36,38 @@ def register_benchmarks(overwrite: bool = False) -> None:
     for bench in [*formulas, *diffs, *lambdas]:
         logger.info(f"Registering ocaml benchmark {bench.name}")
         bench.register(overwrite=overwrite)
+
+def unregister_benchmarks(delete_image: bool = False) -> None:
+    formulas = [
+        Benchmark(
+            f"formula-{i}",
+            f"ghcr.io/kupl/starlab-benchmarks/ocaml:formula-{i}",
+            language_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cat metadata.json | jq -r .language",
+            workdir_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cd $(cat metadata.json | jq -r .buggyPath) && pwd",
+        )
+        for i in range(1, 101)
+    ]
+
+    diffs = [
+        Benchmark(
+            f"diff-{i}",
+            f"ghcr.io/kupl/starlab-benchmarks/ocaml:diff-{i}",
+            language_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cat metadata.json | jq -r .language",
+            workdir_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cd $(cat metadata.json | jq -r .buggyPath) && pwd",
+        )
+        for i in range(1, 101)
+    ]
+
+    lambdas = [
+        Benchmark(
+            f"lambda-{i}",
+            f"ghcr.io/kupl/starlab-benchmarks/ocaml:lambda-{i}",
+            language_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cat metadata.json | jq -r .language",
+            workdir_command="export DEBIAN_FRONTEND=non-interactive && apt-get update >/dev/null 2>&1 && apt-get install -y --no-install-recommends jq >/dev/null 2>&1 && cd $(cat metadata.json | jq -r .buggyPath) && pwd",
+        )
+        for i in range(1, 101)
+    ]
+
+    for bench in [*formulas, *diffs, *lambdas]:
+        logger.info(f"Unregistering ocaml benchmark {bench.name}")
+        bench.unregister(delete_image=delete_image)
